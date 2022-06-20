923318de988caf881d095a452fa4a4f6ebb524d5
freemt <yucongo+fmt@gmail.com>
2022-06-20 17:47:41 +0800

    Bump version from 0.1.0-alpha.0 to 0.1.0-alpha.1

--- tests/test_fetch_radio_cmat2aset.py
+++ tests/test_fetch_radio_cmat2aset.py
@@ -1,7 +1,7 @@
 """Test fetch_radio_cmat2aset."""
 # pylint: disable=broad-except
-from fetch_radio_cmat2aset import __version__
-from fetch_radio_cmat2aset import fetch_radio_cmat2aset
+import joblib
+from fetch_radio_cmat2aset import __version__, fetch_radio_cmat2aset
 
 
 def test_version():
@@ -15,3 +15,27 @@ def test_sanity():
         assert not fetch_radio_cmat2aset()
     except Exception:
         assert True
+
+
+def test_fetch_radio_cmat2aset_simple():
+    """Test fetch_radio_cmat2aset simple."""
+    cmat = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+    ]
+    res = fetch_radio_cmat2aset(
+        cmat,
+        # api_url="http://127.0.0.1:7860/api/predict",
+    )
+    assert res == [[0, 0, 1.0], [1, 1, 1.0], [2, 2, 1.0]]
+
+
+def test_fetch_radio_cmat2aset_84():
+    """Test fetch_radio_cmat2aset simple."""
+    cmat = joblib.load("tests/cmat_list.lzma")
+    res = fetch_radio_cmat2aset(
+        cmat.tolist(),
+        # api_url="http://127.0.0.1:7860/api/predict",
+    )
+    assert len(res) >= 84
